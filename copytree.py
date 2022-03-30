c18bd02c9a41d24e488eba18ebf5c8a0f7462982
gshaw <gshaw@LC5L93B3>
2022-03-30 10:47:26 -0700

    initialize loggers

--- copytree.py
+++ copytree.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import pytesseract
 from datetime import date
 from pdf2image import convert_from_path
@@ -13,6 +14,8 @@ sourceDir = r'\\mryflash\TempDecStore\iPub_Support'
 
 poppler_path = os.path.join(os.path.dirname(__file__), 'poppler-0.68.0_x86/bin')
 custom_config = r'--oem 3 --psm 6'
+
+
 
 
 def processDestDir(top):
@@ -30,7 +33,6 @@ def convert_pdf(src_path, dst_path):
     cmd = "gpcl6win64.exe -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=%s %s" % (dst_path, src_path)
     os.system(cmd)
 
-
 # copies pdf versions from source to dest directories
 def pclToPdf(destDir):              
     
@@ -43,18 +45,15 @@ def pclToPdf(destDir):
         convert_pdf(filePath.strip(), destPathTemp.strip())
         print("done.")
 
-def run(): 
+def run(dateTodayDest): 
     destDir = r'\\mryflash\renocsc$\Notices\A_Notices_for_CS'
-    dateTodayDest = date.today().strftime("%Y%m%d")
     destDir = os.path.join(destDir,dateTodayDest)
     pclToPdf(destDir)
     filterRename(destDir)
 
-
 def deleteFile(filePath):
     if os.path.exists(filePath):
         os.remove(filePath)
-
 
 def filterRename(destDir):
 
@@ -98,9 +97,6 @@ def filterRename(destDir):
                 print("Renamed To ", os.path.basename(newName))
                 os.rename(filePath, newName)
         
-
-
-
 # Finds today's file paths and returns them as a generator
 def filePaths():
 
@@ -124,15 +120,37 @@ def filePaths():
         if "0_CIG_0" in filePath or "Affidavit" in filePath: 
             yield(filePath)
 
-    
+def setup_logger(name, log_file):
+    """To setup as many loggers as you want"""
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
 
-        
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
 
+    return logger    
 
+#fetch today's date for destination folder
+dateTodayDest = date.today().strftime("%Y%m%d")
 
+# Create today's error log
+errorLogDir = os.path.join(os.getcwd(),'logs','errorLogs')
+if not os.path.exists(errorLogDir):
+    os.mkdir(errorLogDir)
+errorLogPath = os.path.join(errorLogDir,'error_' + dateTodayDest + '.log' )
+errorLog = setup_logger('errorLog',errorLogPath)
 
+# Create today's running log
+runningLogDir = os.path.join(os.getcwd(),'logs','runningLogs')
+if not os.path.exists(runningLogDir):
+    os.mkdir(runningLogDir)
+runningLogPath = os.path.join(runningLogDir,'run_' + dateTodayDest + '.log')
+runningLog = setup_logger('runningLog',runningLogPath)
 
-run()
+# run(dateTodayDest)
+
 
 
 
